72c5372e904b3b705337c94670a7a9ac6643e64f
Dave Prutch <prutch.david@gmail.com>
2023-08-16 21:16:18 +0000

    challenge 28 init commit

--- ops_228_Event_Logging_Tool_Part_3.py
+++ ops_228_Event_Logging_Tool_Part_3.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-# Script Name:                  Ops 401d8 Challenge 27
+# Script Name:                  Ops 401d8 Challenge 28
 # Author:                       David Prutch
-# Date of latest revision:      08/15/2023
-# Purpose:                      This Script adds logging functionality to ops_426_Event_Logging_Tool_Part_1.py using the logging library
-#                               Add a log rotation feature based on size
+# Date of latest revision:      08/16/2023
+# Purpose:                      This Script adds logging functionality to ops_426_Event_Logging_Tool_Part_2.py using the logging library
+#                               Use StreamHandler and FileHandler in your Python script.
+#                               FileHandler should write to a local file.
+#                               StreamHandler should output to the terminal.
 
+#Import Libraries
 import logging
 from logging.handlers import RotatingFileHandler
 from scapy.all import sr1, IP, TCP, ICMP
@@ -13,12 +16,24 @@ import sys
 
 # Set up logging with log rotation
 log_file = 'port_scanner.log'
+max_log_size = 5 * 1024 * 1024  # 5 MB
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
-rotating_handler = RotatingFileHandler(filename=log_file, maxBytes=100, backupCount=3)
-rotating_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-logging.getLogger('').addHandler(rotating_handler)
+
+# FileHandler to write log data to a local file
+file_handler = RotatingFileHandler(filename=log_file, maxBytes=max_log_size, backupCount=5)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logging.getLogger('').addHandler(file_handler)
+
+# StreamHandler to output log data to the terminal
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)  # You can set the desired log level for the terminal output
+stream_handler.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
+logging.getLogger('').addHandler(stream_handler)
+
+# Disable the default root logger StreamHandler to prevent duplicate output
+logging.getLogger('').handlers = [file_handler, stream_handler]
 
 # Define Variables
 host = "scanme.nmap.org"
@@ -35,7 +50,6 @@ def scan_port(host_address, port):
 
         if response.haslayer(TCP):
             if response.getlayer(TCP).flags == 0x12:  # SYN-ACK flag
-                send_rst = sr1(IP(dst=host) / TCP(dport=port, flags="R"), verbose=False, timeout=1)
                 logging.info(f"Port {port} is OPEN.")
             elif response.getlayer(TCP).flags == 0x14:  # RST flag
                 logging.info(f"Port {port} is CLOSED.")
