51c885bc1dc2a7e5fccf41a1fed9023deb21dcf5
Dave Prutch <prutch.david@gmail.com>
2023-08-29 17:47:40 +0000

    challenge 37 note changes

--- ops_437_Cookie_Capture.py
+++ ops_437_Cookie_Capture.py
@@ -4,7 +4,10 @@
 # Author:                       David Prutch
 # Date of latest revision:      08/29/2023
 # Purpose:                      This script is taken from a starter template.
-#                               Modifications I have mede begin on line # 
+#                               Modifications I have made begin on line # 39 and include the folowing additions
+                                # - Send the cookie back to the site and receive a HTTP response
+                                # - Generate a .html file to capture the contents of the HTTP response
+                                # - Open it with Firefox
 
 # The below Python script shows one possible method to return the cookie from a site that supports cookies.
 
@@ -33,8 +36,8 @@ def bringforthcookiemonster(): # Because why not!
 
 
 
-# Add here some code to make this script perform the following:
-# - Send the cookie back to the site and receive a HTTP response
+# Additions begin below
+
 # Function to send the cookie and capture the HTTP response
 def send_cookie_and_capture_response():
     targetsite = "http://www.whatarecookies.com/cookietest.asp"
@@ -43,14 +46,13 @@ def send_cookie_and_capture_response():
     response = requests.get(targetsite, headers=headers)
     return response.text
 
-# - Generate a .html file to capture the contents of the HTTP response
 # Function to generate an HTML file and write the HTTP response contents to it
 def generate_html_file(content):
     with open('response.html', 'w') as f:
         f.write(content)
 
-# - Open it with Firefox
 # Function to open the generated HTML file with Firefox
+# Make sure Firefox is the default browser
 def open_html_with_firefox():
     import webbrowser
     webbrowser.get("firefox").open("response.html")
